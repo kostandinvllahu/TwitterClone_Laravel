cd400f9446b7bc39143adf0943ff980d73c391e8
kostandinvllahu <Ghostsechackers@gmail.com>
2021-02-28 14:14:30 +0100

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .bird {
+                position: absolute;
+                margin-bottom: 200px;
+                margin-left: auto;
+                margin-right: 550px;
+                left: 0;
+                bottom: 0;
+                width: 300px;
+            }
+
+            .bird svg{
+                position: absolute;
+                margin-bottom: 200px;
+                margin-left: auto;
+                margin-right: 550px;
+                top: 70%;
+                left: 120%;
+                bottom: -100px;
+                margin-bottom: 90px;
+                width: 250px;
+                transform: translate(-50%, -50%);
+  -ms-transform: translate(-50%, -50%);
+ 
+            }
+
+            .m-b-md {
+               margin-bottom: 30px; 
+            }
+            .bird img{
+                style="-webkit-user-select: none;
+                background-color: hsl(0, 0%, 90%);
+                transition: background-color 300ms;" 
+                position: absolute;
+                left: 0;
+                bottom: 0;
+                margin-bottom: -190px;
+                width: 920px;
+                height: 1000px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <div class="bird">
+            <img src="images\twitter.png">
+           <svg viewBox="0 0 24 24"><g><path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z"></path></g></svg>
+           <div>
+          
+    </body>
+</html>
